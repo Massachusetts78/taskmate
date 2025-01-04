d73584afa3577992e11a4839e23d421708b15680
Massachusetts78 <massachusetts9586@gmail.com>
2025-01-04 23:05:10 +0530

    Update for fallback

--- src/backend/server.mjs
+++ src/backend/server.mjs
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app = express();
 const URI = process.env.MONGO_URI;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
